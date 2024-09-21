a10b06a27f4a265ba794bd25c97c7919e5705668
Haley Vlahos <horseslovehaley@gmail.com>
2024-09-20 21:35:31 -0700

    Reload with new input system

--- Assets/Scripts/Player/PlayerGunAmmo.cs
+++ Assets/Scripts/Player/PlayerGunAmmo.cs
@@ -1,40 +1,77 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
+#endif
 
 public class PlayerGunAmmo : MonoBehaviour
 {
-    [SerializeField] private int maxAmmo;
-    [SerializeField] private TextMeshProUGUI ammoText;
-    
-    private int _curAmmo;
-    public int curAmmo
-    {
-        get => _curAmmo;
-        set
-        {
-            _curAmmo = value;
-            ammoText.text = $"{_curAmmo}/{maxAmmo}";
-        }
-    }
+	[SerializeField] private int maxAmmo;
+	[SerializeField] private InputActionReference actionRef;
+	[SerializeField] private TextMeshProUGUI ammoText;
+	[SerializeField] private GameObject ammoScreen;
+	
+	private int _curAmmo;
+	public int curAmmo
+	{
+		get => _curAmmo;
+		set
+		{
+			_curAmmo = value;
+			ammoText.text = $"{_curAmmo}/{maxAmmo}";
+		}
+	}
 
-    private void Start()
-    {
-        Reload();
-    }
+	private void Start()
+	{
+		ammoScreen.SetActive(false);
+		Reload();
 
-    private void Update()
-    {
-        if (Input.GetKeyUp(KeyCode.R))
-        {
-            Reload();
-        }
-    }
+		actionRef.action.performed += context =>
+		{
+			if (context.interaction is TapInteraction)
+			{
+				Reload();
+			} else if (context.interaction is HoldInteraction)
+			{
+				ammoScreen.SetActive(true);
+			}
+		};
+		actionRef.action.canceled += context =>
+		{
+			if (context.interaction is HoldInteraction)
+			{
+				ammoScreen.SetActive(false);
+			}
+		};
+	}
 
-    private void Reload()
-    {
-        curAmmo = maxAmmo;
-    }
+	private void OnEnable()
+	{
+		actionRef.action.Enable();
+	}
+
+	private void OnDisable()
+	{
+		actionRef.action.Disable();
+	}
+
+	private void Update()
+	{
+		
+	}
+	
+	private void Reload()
+	{
+		curAmmo = maxAmmo;
+	}
+	
+#if ENABLE_INPUT_SYSTEM
+	public void OnReload(InputValue value)
+	{
+		
+	}
+#endif
 }
