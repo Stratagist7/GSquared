31083f9bbc97c8d1a634a309771c59b8d993cea7
Haley Vlahos <horseslovehaley@gmail.com>
2024-12-03 21:12:41 -0800

    Freeze SFX

--- Assets/Scripts/Damage/FreezeHandler.cs
+++ Assets/Scripts/Damage/FreezeHandler.cs
@@ -6,18 +6,25 @@ public class FreezeHandler : MonoBehaviour
 {
 	[SerializeField] private GameObject frozenPrefab;
 	[SerializeField] private GameObject shardPrefab;
+	[SerializeField] private AudioClip[] freezeSounds;
+	[SerializeField] private AudioSource audioSource;
 	
 	private List<Animation> frozen = new List<Animation>();
 
 	public void Freeze()
 	{
 		frozen.Add(Instantiate(frozenPrefab, transform.position, transform.rotation, transform).gameObject.GetComponent<Animation>());
+		audioSource.PlayOneShot(freezeSounds[0]);
 		StartCoroutine(UnFreeze());
 	}
 
 	private IEnumerator UnFreeze()
 	{
 		yield return new WaitForSeconds(ReactionValues.FREEZE_TIME);
+		if (frozen.Count == 1)
+		{
+			audioSource.PlayOneShot(freezeSounds[1]);
+		}
 		frozen[0].Play("UnFreeze");
 		Instantiate(shardPrefab, transform.position, frozenPrefab.transform.rotation);
 		while (frozen[0].isPlaying)
