fbe44306ae876dfc0e1b27280fbdd9ea19084697
Haley Vlahos <horseslovehaley@gmail.com>
2024-12-09 22:40:16 -0800

    Model + Animation Update WIP

--- Assets/Scripts/Player/PlayerAnimationEvents.cs
+++ Assets/Scripts/Player/PlayerAnimationEvents.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class PlayerAnimationEvents : MonoBehaviour
 {
@@ -8,6 +9,12 @@ public class PlayerAnimationEvents : MonoBehaviour
     
     [SerializeField] private PlayerGunAmmo ammo;
     [SerializeField] private Animator gunAnim;
+    [Space]
+    [SerializeField] private Transform ammoCartridge;
+    [SerializeField] private Transform gun;
+    [SerializeField] private Transform handLBone;
+    [SerializeField] private Vector3 gunOffset;
+    [SerializeField] private Vector3 handOffset;
 
     public void ResetAmmo()
     {
@@ -18,5 +25,15 @@ public class PlayerAnimationEvents : MonoBehaviour
     {
         gunAnim.SetTrigger(reloadKey);
         PlayerSoundManager.instance.PlayReload();
+    }
+
+    public void ParentGun()
+    {
+        ammoCartridge.parent = gun;
+    }
+
+    public void ParentHand()
+    {
+        ammoCartridge.parent = handLBone;
     }
 }
