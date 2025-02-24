9076bda1d5aedc6b64ba2b58e22a3a3c6717ecc1
Haley Vlahos <horseslovehaley@gmail.com>
2025-02-23 19:10:10 -0800

    Title screen & Animation

--- Assets/Scripts/UI/MainMenuUI.cs
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -1,8 +1,29 @@
+using System;
+using Cinemachine;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuUI : MonoBehaviour
 {
+	[SerializeField] private Animation anim;
+	[SerializeField] private CinemachineDollyCart dollyCart;
+	private bool hasAnimated = false;
+
+	private void Update()
+	{
+		if (hasAnimated)
+		{
+			return;
+		}
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			hasAnimated = true;
+			dollyCart.m_Speed = 2;
+			anim.Play();
+		}
+	}
+
 	public void StartButton()
 	{
 		SceneManager.LoadScene("IntroLevel");
