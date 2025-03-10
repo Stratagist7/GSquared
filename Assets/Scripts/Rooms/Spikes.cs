75bf521413cc14398101f508eac288a036690800
Haley Vlahos <horseslovehaley@gmail.com>
2025-03-10 01:24:11 -0700

    Lava Exterior

--- Assets/Scripts/Rooms/Spikes.cs
+++ Assets/Scripts/Rooms/Spikes.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
+    [SerializeField] private int damage = 10;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            Damageable.Player.GetComponent<PlayerHealth>().Damage(10);
+            Damageable.Player.GetComponent<PlayerHealth>().Damage(damage);
         }
     }
 }
